9707f1b38a30646ed35b114437d2a5ae9fbe1791
wenead99 <42309414+wenead99@users.noreply.github.com>
2019-05-27 16:46:27 +0800

    0.2 beta 更新
    2019.5.27更新：
    1.支持多媒体格式
    2.更智能的程序终止机制
    3.改善程序源码本体

--- AV_Data_Capture.py
+++ AV_Data_Capture.py
@@ -2,12 +2,38 @@ import glob
 import os
 import time
 
-#a=glob.glob(os.getcwd()+r"\*.py")
-a=glob.glob(os.getcwd()+r"\*\**\*.mp4")
-b=glob.glob(os.getcwd()+r"\*.mp4")
-for i in b:
-    a.append(i)
+def movie_lists():
+    #MP4
+    a1 = glob.glob(os.getcwd() + r"\*\**\*.mp4")
+    a2 = glob.glob(os.getcwd() + r"\*.mp4")
+    # AVI
+    b1 = glob.glob(os.getcwd() + r"\*\**\*.avi")
+    b2 = glob.glob(os.getcwd() + r"\*.avi")
+    # RMVB
+    c1 = glob.glob(os.getcwd() + r"\*\**\*.rmvb")
+    c2 = glob.glob(os.getcwd() + r"\*.rmvb")
+    # WMV
+    d1 = glob.glob(os.getcwd() + r"\*\**\*.wmv")
+    d2 = glob.glob(os.getcwd() + r"\*.wmv")
+    # MOV
+    e1 = glob.glob(os.getcwd() + r"\*\**\*.mov")
+    e2 = glob.glob(os.getcwd() + r"\*.mov")
+    # MKV
+    f1 = glob.glob(os.getcwd() + r"\*\**\*.mkv")
+    f2 = glob.glob(os.getcwd() + r"\*.mkv")
+    # FLV
+    g1 = glob.glob(os.getcwd() + r"\*\**\*.flv")
+    g2 = glob.glob(os.getcwd() + r"\*.flv")
+
+    total = a1+a2+b1+b2+c1+c2+d1+d2+e1+e2+f1+f2+g1+g2
+    return total
+
+def lists_from_test(custom_nuber):
+    a=[]
+    a.append(custom_nuber)
+    return a
 
 os.chdir(os.getcwd())
-for i in a:
+for i in movie_lists():
     os.system('python core.py'+' "'+i+'"')
+print("[+]All finished!!!")
