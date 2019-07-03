f0437cf6af39d06299845b026dff754cc9e54690
Yoshiko <42309414+yoshiko2@users.noreply.github.com>
2019-07-04 03:01:18 +0800

    Delete py to exe.bat

--- py to exe.bat
+++
@@ -1,2 +0,0 @@
-pyinstaller --onefile AV_Data_Capture.py 
-pyinstaller --onefile core.py --hidden-import ADC_function.py --hidden-import fc2fans_club.py --hidden-import javbus.py --hidden-import siro.py
