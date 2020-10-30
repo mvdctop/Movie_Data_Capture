c1039872b9e8cfd0fe79aebd6c0a14e97abb483a
Yoshiko2 <42309414+yoshiko2@users.noreply.github.com>
2020-10-30 20:47:14 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,3 +9,7 @@ apt upgrade -y
 pip3 install --upgrade -r requirements.txt
 make
 mv bin/* /avdc_bin
+version=$(cat AV_Data_Capture.py | grep "version = " | tr -d "version = '")
+
+cd /avdc_bin
+zip AV_Data_Capture-CLI-$(version)-linux-$(uname -m).zip AV_Data_Capture config.ini
