86db4b132dca5b846f1c261cdca294419f156a30
Yoshiko2 <yoshiko2.github@gmail.com>
2020-12-19 23:31:44 +0800

    Create FreeBSD-amd64.sh

---
+++ wrapper/FreeBSD-amd64.sh
@@ -0,0 +1,5 @@
+pkg install python37 py37-requests py37-pip py37-lxml py37-pillow py37-cloudscraper py37-pysocks git zip py37-pyinstaller py37-beautifulsoup448
+pip install pyquery
+cd ../
+chmox +x encapsulation.sh
+./encapsulation.sh
