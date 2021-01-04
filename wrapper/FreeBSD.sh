4ea5e9d6cb2a6e02e5a0be1f51e5f8af195cf289
Yoshiko2 <yoshiko2.github@gmail.com>
2021-01-04 15:35:03 +0800

    Update FreeBSD.sh

--- wrapper/FreeBSD.sh
+++ wrapper/FreeBSD.sh
@@ -1,4 +1,4 @@
 pkg install python37 py37-requests py37-pip py37-lxml py37-pillow py37-cloudscraper py37-pysocks git zip py37-pyinstaller py37-beautifulsoup448
 pip install pyquery
-pyinstaller --onefile AV_Data_Capture.py  --hidden-import ADC_function.py --hidden-import core.py --add-data "$(python -c 'import cloudscraper as _; print(_.__path__[0])' | tail -n 1):cloudscraper"
+pyinstaller --onefile AV_Data_Capture.py  --hidden-import ADC_function.py --hidden-import core.py --add-data "$(python3.7 -c 'import cloudscraper as _; print(_.__path__[0])' | tail -n 1):cloudscraper"
 cp config.ini ./dist
