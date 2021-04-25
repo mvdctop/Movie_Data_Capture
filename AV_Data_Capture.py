746602f6d6396ab9a6ede5466fb1712d94c115d1
yoshiko2 <wenead99@gmail.com>
2021-04-25 20:03:56 +0800

    Update to 4.5.2

--- AV_Data_Capture.py
+++ AV_Data_Capture.py
@@ -133,7 +133,7 @@ def create_data_and_move_with_custom_number(file_path: str, c: config.Config, cu
 
 
 if __name__ == '__main__':
-    version = '4.5.1'
+    version = '4.5.2'
 
     # Parse command line args
     single_file_path, folder_path, config_file, custom_number, auto_exit = argparse_function(version)
