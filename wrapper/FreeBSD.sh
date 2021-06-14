4c1a0f80c12fff07873818b66987bc6c5b37d1e7
lededev <lededev@noreplay.github.com>
2021-06-14 12:28:39 +0800

    improve processing speed by priority of using local watermark images

--- wrapper/FreeBSD.sh
+++ wrapper/FreeBSD.sh
@@ -1,4 +1,4 @@
 pkg install python38 py38-requests py38-pip py38-lxml py38-pillow py38-cloudscraper py38-pysocks git zip py38-beautifulsoup448
 pip install pyquery pyinstaller
-pyinstaller --onefile AV_Data_Capture.py  --hidden-import ADC_function.py --hidden-import core.py --add-data "$(python3.8 -c 'import cloudscraper as _; print(_.__path__[0])' | tail -n 1):cloudscraper"
+pyinstaller --onefile AV_Data_Capture.py  --hidden-import ADC_function.py --hidden-import core.py --add-data "$(python3.8 -c 'import cloudscraper as _; print(_.__path__[0])' | tail -n 1):cloudscraper" --add-data "Img:Img"
 cp config.ini ./dist
