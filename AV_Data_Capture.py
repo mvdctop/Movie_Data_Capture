a98ba439bc00894c6a121161a4e6770ba149ab74
lededev <lededev@noreplay.github.com>
2021-09-03 18:49:52 +0800

    ignore -trailer video files

--- AV_Data_Capture.py
+++ AV_Data_Capture.py
@@ -57,7 +57,7 @@ def movie_lists(root, escape_folder):
             total += movie_lists(f, escape_folder)
         elif os.path.splitext(f)[1].upper() in file_type:
             absf = os.path.abspath(f)
-            if conf.main_mode() == 3 or not is_link(absf):
+            if conf.main_mode() == 3 or (not is_link(absf) and not re.match(r'.*-trailer\..*', f, re.IGNORECASE)):
                 total.append(absf)
     return total
 
