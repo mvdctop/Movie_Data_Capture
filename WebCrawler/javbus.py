b64dc83e2e8ec165ef680f731a5403bf0ebeedfc
lededev <lededev@noreplay.github.com>
2021-11-14 08:58:27 +0800

    simp

--- WebCrawler/javbus.py
+++ WebCrawler/javbus.py
@@ -72,7 +72,7 @@ def getSerise(html):   #获取系列
     return str(x[0]) if len(x) else ''
 def getTag(html):  # 获取标签
     klist = html.xpath('/html/head/meta[@name="keywords"]/@content')[0].split(',')
-    return [v for v in klist[1:]]
+    return klist[1:]
 def getExtrafanart(htmlcode):  # 获取剧照
     html_pather = re.compile(r'<div id=\"sample-waterfall\">[\s\S]*?</div></a>\s*?</div>')
     html = html_pather.search(htmlcode)
