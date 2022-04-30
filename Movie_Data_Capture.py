107d5857ddf4fa3c04da0f0adf1a33dda055e867
Yoshiko2 <yoshiko2.dev@gmail.com>
2022-04-30 20:09:15 +0800

    Update 6.1.2

--- Movie_Data_Capture.py
+++ Movie_Data_Capture.py
@@ -674,7 +674,7 @@ def period(delta, pattern):
 
 
 if __name__ == '__main__':
-    version = '6.1.1'
+    version = '6.1.2'
     urllib3.disable_warnings()  # Ignore http proxy warning
     app_start = time.time()
 
