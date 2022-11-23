8ec6018eb1d8d1fea6d74a4906b6f2734319a1ca
Marks <marks5231@outlook.com>
2022-11-22 16:09:37 -0800

    fix white space

--- scrapinglib/storyline.py
+++ scrapinglib/storyline.py
@@ -218,7 +218,7 @@ def getStoryline_avno1(number, debug, proxies, verify):  #获取剧情介绍 从
         partial_num = bool(re.match(r'\d{6}[\-_]\d{2,3}', number))
         for title, desc in zip(titles, descs):
             page_number = title[title.rfind(' ')+1:].strip()
-            if not partial_num :
+            if not partial_num:
                 # 不选择title中带破坏版的简介
                 if re.match(f'^{number}$', page_number, re.I) and title.rfind('破坏版')== -1:
                     return desc.strip()
