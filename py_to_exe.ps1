cb920d7163c89446a40f068663a1aa289937aeca
yoshiko2 <yoshiko2.dev@gmail.com>
2023-05-08 05:20:26 +0800

    Fix face recognition in compile

--- py_to_exe.ps1
+++ py_to_exe.ps1
@@ -1,21 +1,22 @@
 # If you can't run this script, please execute the following command in PowerShell.
 # Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
-# bugfix：set submodules find path
-$Env:PYTHONPATH=$pwd.path
-$PYTHONPATH=$pwd.path
+$CLOUDSCRAPER_PATH = $( python -c 'import cloudscraper as _; print(_.__path__[0])' | select -Last 1 )
+$OPENCC_PATH = $( python -c 'import opencc as _; print(_.__path__[0])' | select -Last 1 )
+$FACE_RECOGNITION_MODELS = $( python -c 'import face_recognition_models as _; print(_.__path__[0])' | select -Last 1 )
+
 mkdir build
 mkdir __pycache__
 
-pyinstaller --collect-submodules "ImageProcessing" `
-    --collect-data "face_recognition_models" `
-    --collect-data "cloudscraper" `
-    --collect-data "opencc" `
+pyinstaller --onefile Movie_Data_Capture.py `
+    --hidden-import "ImageProcessing.cnn" `
+    --python-option u `
+    --add-data "$FACE_RECOGNITION_MODELS;face_recognition_models" `
+    --add-data "$CLOUDSCRAPER_PATH;cloudscraper" `
+    --add-data "$OPENCC_PATH;opencc" `
     --add-data "Img;Img" `
     --add-data "config.ini;." `
     --add-data "scrapinglib;scrapinglib" `
-    --onefile Movie_Data_Capture.py
-
 
 rmdir -Recurse -Force build
 rmdir -Recurse -Force __pycache__
